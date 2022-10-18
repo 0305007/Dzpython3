b711cdbf7a08690279c8d6c99192ffb2af59165e
Мурат <0305007@mail.ru>
2022-10-18 19:00:33 +0300

    init

--- hello.py
+++ hello.py
@@ -307,23 +307,98 @@ from unittest.mock import patch
 
 
 
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# num = int(input("Введите число: "))
+# i = 2 # первое простое число
+# lst = []
+# old = num
+# while i <= num:
+#     if num % i == 0:
+#         lst.append(i)
+#         num //= i
+#         i = 2
+#     else:
+#         i += 1
+# print(f"Простые множители числа {old} приведены в списке: {lst}")
 
 
 
 
+# Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# print(f"Исходный список: {lst}")
+# new_lst = []
+# [new_lst.append(i) for i in lst if i not in new_lst]
+# print(f"Список из неповторяющихся элементов: {new_lst}")
 
 
 
 
+# Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k. 
+# Пример:
+# k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+
+# import random
+
+
+# def write_file(st):
+#     with open('file33.txt', 'w') as data:
+#         data.write(st)
+
+
+# def rnd():
+#     return random.randint(0,101)
+
+
+# def create_mn(k):
+#     lst = [rnd() for i in range(k+1)]
+#     return lst
+    
+
+# def create_str(sp):
+#     lst= sp[::-1]
+#     wr = ''
+#     if len(lst) < 1:
+#         wr = 'x = 0'
+#     else:
+#         for i in range(len(lst)):
+#             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+#                 wr += f'{lst[i]}x^{len(lst)-i-1}'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 2 and lst[i] != 0:
+#                 wr += f'{lst[i]}x'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 1 and lst[i] != 0:
+#                 wr += f'{lst[i]} = 0'
+#             elif i == len(lst) - 1 and lst[i] == 0:
+#                 wr += ' = 0'
+#     return wr
+
+# k = int(input("Введите натуральную степень k = "))
+# koef = create_mn(k)
+# write_file(create_str(koef))
 
 
 
 
+# Задайте два целых числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
 
-
-
-
-
+# x, y = 4, 6
+# if x > y: big_num = x
+# else: big_num = y
+# while(True):
+#     if (big_num % x == 0) and (big_num % y == 0):
+#         result = big_num
+#         break
+#     big_num += 1
+# print(result)
 
 
 
